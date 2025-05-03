8a10120da65d34a14bca86fbb89d5a23d23a586c
Adriel007 <adrielsouzaandrade8@gmail.com>
2025-05-03 11:22:12 -0300

    charts update

--- src/renderer/scripts.js
+++ src/renderer/scripts.js
@@ -9,33 +9,14 @@ document.addEventListener("DOMContentLoaded", () => {
     "ipcHandlers",
     "config",
   ];
+  scripts.forEach((script) => {
+    const scriptElement = document.createElement("script");
+    scriptElement.src = `./modules/${script}.js`;
+    document.body.appendChild(scriptElement);
+  });
 
-  const loadScript = (src) => {
-    return new Promise((resolve, reject) => {
-      const scriptElement = document.createElement("script");
-      scriptElement.src = `./modules/${src}.js`;
-      scriptElement.defer = true;
-      scriptElement.onload = resolve;
-      scriptElement.onerror = () =>
-        reject(new Error(`Erro ao carregar ${src}.js`));
-      document.body.appendChild(scriptElement);
-    });
-  };
-
-  // Aguarda o carregamento de todos os scripts
-  Promise.all(scripts.map(loadScript))
-    .then(() => {
-      // Após o carregamento bem-sucedido de todos os scripts
-      document.getElementById("loading").classList.add("hidden");
-      document.getElementById("container").classList.remove("hidden");
-    })
-    .catch((error) => {
-      console.error("Erro ao carregar scripts:", error);
-      Swal.fire({
-        title: "Erro",
-        text: "Erro ao carregar scripts da aplicação.",
-        icon: "error",
-        confirmButtonText: "OK",
-      });
-    });
+  setTimeout(() => {
+    document.getElementById("loading").classList.add("hidden");
+    document.getElementById("container").classList.remove("hidden");
+  }, 2_000);
 });
