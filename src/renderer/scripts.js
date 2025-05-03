b6d8b7b3f8681fc1e5d38fe6c1e28a7c68555c5b
Adriel007 <adrielsouzaandrade8@gmail.com>
2025-05-03 11:40:22 -0300

    charts update

--- src/renderer/scripts.js
+++ src/renderer/scripts.js
@@ -1,20 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const scripts = [
-    "alerts",
-    "charts",
-    "dataProcessing",
-    "theme",
-    "tabs",
-    "fileHandlers",
-    "ipcHandlers",
-    "config",
-  ];
-  scripts.forEach((script) => {
-    const scriptElement = document.createElement("script");
-    scriptElement.src = `./modules/${script}.js`;
-    document.body.appendChild(scriptElement);
-  });
-
   setTimeout(() => {
     document.getElementById("loading").classList.add("hidden");
     document.getElementById("container").classList.remove("hidden");
