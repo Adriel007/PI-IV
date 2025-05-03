3b11ae76e9cb260417800cddeddac759145a4112
Adriel007 <adrielsouzaandrade8@gmail.com>
2025-05-03 11:15:50 -0300

    loading ifx

--- src/renderer/scripts.js
+++ src/renderer/scripts.js
@@ -9,15 +9,33 @@ document.addEventListener("DOMContentLoaded", () => {
     "ipcHandlers",
     "config",
   ];
-  scripts.forEach((script) => {
-    const scriptElement = document.createElement("script");
-    scriptElement.src = `./modules/${script}.js`;
-    scriptElement.defer = true;
-    document.body.appendChild(scriptElement);
-  });
 
-  setTimeout(() => {
-    document.getElementById("loading").classList.add("hidden");
-    document.getElementById("container").classList.remove("hidden");
-  }, 1_000);
+  const loadScript = (src) => {
+    return new Promise((resolve, reject) => {
+      const scriptElement = document.createElement("script");
+      scriptElement.src = `./modules/${src}.js`;
+      scriptElement.defer = true;
+      scriptElement.onload = resolve;
+      scriptElement.onerror = () =>
+        reject(new Error(`Erro ao carregar ${src}.js`));
+      document.body.appendChild(scriptElement);
+    });
+  };
+
+  // Aguarda o carregamento de todos os scripts
+  Promise.all(scripts.map(loadScript))
+    .then(() => {
+      // Após o carregamento bem-sucedido de todos os scripts
+      document.getElementById("loading").classList.add("hidden");
+      document.getElementById("container").classList.remove("hidden");
+    })
+    .catch((error) => {
+      console.error("Erro ao carregar scripts:", error);
+      Swal.fire({
+        title: "Erro",
+        text: "Erro ao carregar scripts da aplicação.",
+        icon: "error",
+        confirmButtonText: "OK",
+      });
+    });
 });
